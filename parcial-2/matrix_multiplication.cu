02e1271fb0fcfb98fa68efd4313729fbdf87d18f
Oscar E. Bernal <oscarbernal93@gmail.com>
2018-04-13 10:29:04 -0500

    added gitignore and example files

--- parcial-2/matrix_multiplication.cu
+++ parcial-2/matrix_multiplication.cu
@@ -4,7 +4,7 @@
 int main(int argc, char **argv)
 {
 	if( 3 != argc ){
-		printf("Incorrect number of params: %i\n",argc - 1)
+		printf("Incorrect number of params: %i\n",argc - 1);
 		printf("Please include the filename of the two files\n");
 		return 1;
 	}
